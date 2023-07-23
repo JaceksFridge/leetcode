b13a01955ac95f0d370ccf50f57be9f55ed21d39
JaceksFridge <Japacz22@gmail.com>
2023-07-23 14:52:25 +0200

    2469

---
+++ easy/ConvertTheTemperature-2469.js
@@ -0,0 +1,15 @@
+
+
+const celsius = 36.50
+
+
+var convertTemperature = function(celsius) {
+    
+    let kelvin = (celsius + 273.15)
+    let farenheit = (celsius * 1.80 + 32)
+
+    return temps = [kelvin, farenheit]
+};
+
+
+console.log(convertTemperature(celsius))
