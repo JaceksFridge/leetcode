bad3bfb930f8ce16890e5ad5bf9fc13eeab609f0
JaceksFridge <Japacz22@gmail.com>
2023-07-24 13:35:41 +0200

    2727

---
+++ JS30/JSON/IsObjectEmpty-2727.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+var isEmpty = function(obj) {
+
+    if (Object.keys(obj2).length == 0) {
+        return true
+    } else {
+        return false
+    }
+};
+
+
+console.log(isEmpty(obj))
+
+
