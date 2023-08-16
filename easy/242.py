c38cd9dc2070a00536f7ba218fb3192fd6f4e753
JaceksFridge <Japacz22@gmail.com>
2023-08-16 11:14:45 +0200

    242

--- easy/242.py
+++ easy/242.py
@@ -5,18 +5,9 @@ t = "nagaram"
 class Solution(object):
     def isAnagram(self, s, t):
         
-        if len(s) != len(t):
-            return False
-        
-        s_set = set()
-        t_set = set()
-        
-        for i in range(len(s)):
-            s_set.add(s[i])
-            t_set.add(t[i])
-
-        if sorted(s_set) == sorted(t_set):
+        if sorted(s) == sorted(t):
             return True
+
 
 
 sol = Solution()
