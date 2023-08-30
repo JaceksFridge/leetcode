268d41cc66799613e31cb64ef6bdf16c12de1604
JaceksFridge <Japacz22@gmail.com>
2023-08-31 01:30:33 +0200

    1669

--- easy/1661.py
+++ easy/1661.py
@@ -1,20 +1,32 @@
 
 
-word1  = ["abc", "d", "defg"]
-word2 = ["abcddefg"]
+# word1  = ["abc", "d", "defg"]
+# word2 = ["abcddefg"]
+
+# class Solution(object):
+#     def arrayStringsAreEqual(self, word1, word2):
+        
+#         master1 = ""
+#         master2 = ""
+        
+#         for s in word1:
+#             master1 += s
+#         for s in word2:
+#             master2 += s
+            
+#         return master1 == master2
+        
+# sol = Solution()
+# print(sol.arrayStringsAreEqual(word1, word2))
+
+
+
 
 class Solution(object):
     def arrayStringsAreEqual(self, word1, word2):
-        
-        master1 = ""
-        master2 = ""
-        
-        for s in word1:
-            master1 += s
-        for s in word2:
-            master2 += s
-            
-        return master1 == master2
+
+         
+        return "".join(word1) == "".join(word2)
         
 sol = Solution()
 print(sol.arrayStringsAreEqual(word1, word2))
