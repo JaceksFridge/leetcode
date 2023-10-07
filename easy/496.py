3807945d37d10a3072d710428cce52d9d06b6a1b
JaceksFridge <Japacz22@gmail.com>
2023-10-07 12:14:54 +0200

    496

--- easy/496.py
+++ easy/496.py
@@ -1,34 +1,62 @@
 
 
-nums1 = [2,4]
-nums2 = [1,2,3,4]
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
+
 
 
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
         
-        tresor = []
         
-        for num in nums1:
-            
-            search = False
-            found = False
-            
-            for number in nums2:
-                
-                if num == number:
-                    search = True
-                    
-                elif search and number > num:
-                    found = True
-                    tresor.append(number)
-                    break
-                
-            if not found:
-                tresor.append(-1)
-                
+        hashmap = {}
+        stack = []
+        tresor = [-1] * len(nums1)
+        
+        for i, val in enumerate(nums1):
+            hashmap[val] = i
+        
+        for idx, num in enumerate(nums2):
+            while stack and num > stack[-1]:
+                value = stack.pop()
+                index = hashmap[value]
+                tresor[index] = num
+            if num in nums1:
+                stack.append(num)
         return tresor
-
+        
+        
 
 s = Solution()
-print(s.nextGreaterElement(nums1, nums2))
+s.nextGreaterElement(nums1, nums2)
+
+
+
+# class Solution(object):
+#     def nextGreaterElement(self, nums1, nums2):
+        
+#         tresor = []
+        
+#         for num in nums1:
+            
+#             search = False
+#             found = False
+            
+#             for number in nums2:
+                
+#                 if num == number:
+#                     search = True
+                    
+#                 elif search and number > num:
+#                     found = True
+#                     tresor.append(number)
+#                     break
+                
+#             if not found:
+#                 tresor.append(-1)
+                
+#         return tresor
+
+
+# s = Solution()
+# s.nextGreaterElement(nums1, nums2)
