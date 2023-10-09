58b787e2de7c1ef46ab75990d24a7a5b734134e7
JaceksFridge <Japacz22@gmail.com>
2023-10-09 10:48:08 +0200

    2619

---
+++ easy/2619.js
@@ -0,0 +1,20 @@
+
+const nums = [null, {}, 3]
+
+Array.prototype.last = function() {
+
+    if (this.length === 0) {
+        return -1
+    } else {
+        return this.reverse()[0]
+    }
+};
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+
+
+nums.last()
+
