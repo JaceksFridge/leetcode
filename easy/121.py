e654e44c728aaa07f118341dcad596bc0a6a1dbc
JaceksFridge <Japacz22@gmail.com>
2023-10-16 14:02:09 +0200

    made sliding window work

--- easy/121.py
+++ easy/121.py
@@ -7,11 +7,17 @@ k = 3
 arr = [ 1, 2, 3, 4, 5, 6 ]
 
 ### get sum of all subarrays length k
+results = []
 
 for i in range(0, len(arr) -k +1):
     
     sum = 0
-    for j in range(i, i+k):
-        sum += arr[j]
+    if i == 0:
+        for j in range(i, i+k):
+            sum += arr[j]
+        results.append(sum)
+    else: 
+        sum = results[-1] -i + i+k
+        results.append(sum)
 
-    print(sum)
+print(results)
