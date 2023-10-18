4fb490e2d140de5cff608a3780b84af910e7ffab
JaceksFridge <Japacz22@gmail.com>
2023-10-18 11:41:05 +0200

    1876

--- easy/1876.py
+++ easy/1876.py
@@ -5,15 +5,24 @@ s = "xyzzaz"
 class Solution(object):
     def countGoodSubstrings(self, s):
         
-        count = 0
+
         start = 0
         end = 2
-        
-        tresor = []
-        
-        for i in range(0, len(s) - 3 + 1):
-            re
+        count = 0
+    
+        while (end <= len(s)):
             
+            substring = s[start:end+1]
+            
+            if len(set(substring)) == 3:
+                count += 1
+            
+            start += 1
+            end += 1
         
+        
+        return count
+        
+
 sol = Solution()
-sol.countGoodSubstrings(s)
+print(sol.countGoodSubstrings(s))
