5407bd65bd58b082cd35161a97ce082b3be0d768
JaceksFridge <Japacz22@gmail.com>
2023-10-18 10:41:58 +0200

    58

--- easy/58.py
+++ easy/58.py
@@ -1,13 +1,33 @@
 
 
+# class Solution(object):
+#     def lengthOfLastWord(self, s):
+
+#         words = s.strip().split(" ")
+#         the_word = words[-1]
+#         return len(the_word)
+
+
+# sol = Solution()
+# sol.lengthOfLastWord(s)
+        
+        
+        
+
+
 class Solution(object):
     def lengthOfLastWord(self, s):
 
-        words = s.strip().split(" ")
-        the_word = words[-1]
-        return len(the_word)
+        count = 0
+        i = len(s) -1
+        
+        while (i >= 0 and s[i] == ' '):
+            i -= 1
+        while (i >=0 and s[i] !=  ' '):
+            i -= 1
+            count += 1
+        return count
 
 
 sol = Solution()
 sol.lengthOfLastWord(s)
-        
