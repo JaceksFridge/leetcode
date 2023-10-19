30e4896c043cb8c5aa3118c90db679dbd656d867
JaceksFridge <Japacz22@gmail.com>
2023-10-19 10:52:30 +0200

    1137

---
+++ easy/1137.py
@@ -0,0 +1,27 @@
+
+n = 6
+
+class Solution(object):
+    def tribonacci(self, n):
+
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            arr = [0, 1, 1]
+            
+            for i in range(len(arr), n):
+                
+                next = arr[-1] + arr[-2] + arr[-3]
+                arr.append(next)
+                
+            
+            return arr[-3] + arr[-2] + arr[-1]
+    
+    
+s = Solution()    
+s.tribonacci(n)
+
