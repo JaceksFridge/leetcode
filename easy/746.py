d9b6f9b393a57491946fcb648858a96e57a664d6
JaceksFridge <Japacz22@gmail.com>
2023-11-16 08:02:27 +0100

    746

--- easy/746.py
+++ easy/746.py
@@ -1,40 +1,16 @@
 
 
-cost = [0,1,2,2]
+cost = [10,45,30,10,15,20]
 
 class Solution(object):
     def minCostClimbingStairs(self, cost):
 
-
-        spot = 0
-        if len(cost) == 3:
-            spot = 1
-        elif cost[1] <= cost[0]:
-            spot = 1
-        else:
-            spot = 0
+        cost.append(0) 
             
-        counter = cost[spot]
-
-
-        while spot + 2 < len(cost):
+        for i in range(len(cost) -3, -1, -1):
+            cost[i] += min(cost[i+1], cost[i+2])
             
-            if spot+2 == len(cost) - 2:
-                if cost[spot+2] < (cost[spot+1] + cost[-1]):
-                    counter += cost[spot+2]
-                    return counter
-            
-            single = cost[spot+1]
-            double = cost[spot+2]
-        
-            if double <= single:
-                spot += 2
-            else:
-                spot += 1
-            
-            counter += cost[spot]
-            
-        return counter
+        return min(cost[0], cost[1])
     
 s = Solution()
 print(s.minCostClimbingStairs(cost))
